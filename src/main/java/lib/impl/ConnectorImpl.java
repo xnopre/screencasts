2536e32fc00964091aeb697945d48f72824fce9f
Xavier NOPRE <xnopre@gmail.com>
2018-05-02 18:53:25 +0200

    Création du squelette vide pour coder à partir de master

--- src/main/java/lib/impl/ConnectorImpl.java
+++
@@ -1,9 +0,0 @@
-package lib.impl;
-
-import lib.Connector;
-
-public class ConnectorImpl implements Connector {
-    public void sendFrame(String frame) {
-        throw new UnsupportedOperationException("Not implemented here ;-)");
-    }
-}
